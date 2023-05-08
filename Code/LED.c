f09c95f216e1109078050a4902e68d415742c709
mmyrberg <m.myrberg@outlook.com>
2023-05-08 12:13:22 +0200

    Update comments in LED files

--- Code/LED.c
+++ Code/LED.c
@@ -1,151 +1,145 @@
-include "LED.h" //Inkluderar headerfilen för att även denna fil skall känna till de olika LED-beteckningarna och funktioner
+#include "LED.h" //Inkludera headerfilen för att denna fil skall känna till dem olika LED-beteckningarna och funktionerna.
 
-//Konstruktorn för LED-lamporna med typ, färg och status
-void Led_ctor(Led_Type * const me, LedColor_Type _color, LedState_Type _state){
-  
-	me->color  = _color;
-	me->state  = _state; 
+//Konstruktorn för LED-lamporna. Sätter lampornas initiala värden för färg och status.
+void Led_ctor(Led_Type *const me, LedColor_Type _color, LedState_Type _state) {
 
-	//Enablea klockan för LED-porten (GPIOB)
-	RCC->AHB1ENR |= LED_PORT_CLOCK;  
-	
-	//Konfiguera LED-pinsen baserad på deras färg och status
-	switch(_color){
-		 
-		case RED: //Sätta portläget för LED-konfigurationen till output
-			LED_PORT->MODER |=LED_RED_MODE_BIT;
-		  if(me->state  == ON){ //Sätter på LED
-			   LED_PORT->ODR |= LED_RED_PIN;
-				printf("The RED Led is set ON \n\r");
-			}
-			else{ //Stänger av LED
-			  LED_PORT->ODR &= ~LED_RED_PIN;
-        printf("The RED Led is set OFF \n\r");
+    //Sätter färg och status för LED-lampan som me pekar på till de värden som skickas med som argument i _color och _state.
+    me->color  = _color;
+    me->state  = _state; 
 
-			}
-		break;
-		
-		case YELLOW: //Sätta portläget för LED-konfigurationen till output
-			LED_PORT->MODER |=LED_YELLOW_MODE_BIT;
-		  if(me->state  == ON){//Sätter på LED
-			   LED_PORT->ODR |= LED_YELLOW_PIN;
-				printf("The YELLOW Led is set ON \n\r");
-			}
-			else{//Stänger av LED
-			  LED_PORT->ODR &= ~LED_YELLOW_PIN;
-        printf("The YELLOW Led is set OFF \n\r");
+    //Aktiverar klockan för GPIO-porten som LED-lampan är ansluten till
+    RCC->AHB1ENR |= LED_PORT_CLOCK;  
 
-			}
-		break;
-		
-		case BLUE: //Sätta portläget för LED-konfigurationen till output
-			LED_PORT->MODER |=LED_BLUE_MODE_BIT;
-		  if(me->state  == ON){//Sätter på LED
-			   LED_PORT->ODR |= LED_BLUE_PIN;
-				printf("The BLUE Led is set ON \n\r");
-			}
-			else{//Stänger av LED
-			  LED_PORT->ODR &= ~LED_BLUE_PIN;
-        printf("The BLUE Led is set OFF \n\r");
+    //Switch-sats för att konfiguera LED-pinsen beroende på deras färg och status
+    switch(_color){
+        case RED: //Om färgen är röd...
+            LED_PORT->MODER |= LED_RED_MODE_BIT; //...ställ in moder-registret med LED_RED_MODE_BIT för att enbart den bit som representerar den röda LED-pinsen blir konfiguerad som en utgångsport (output). 
+            if(me->state == ON){ //Om state är ON (1)...
+                LED_PORT->ODR |= LED_RED_PIN; //...slå på LED-lampan genom att ändra status i ODR-registret till aktiv för den röda LED-pinsen...
+                printf("The RED Led is set ON \n\r"); //...och skriv ut att den röda LED:n är påslagen.
+            }
+            else{ //Annars, state är OFF (0)...
+                LED_PORT->ODR &= ~LED_RED_PIN; //...stäng av LED-lampan genom att ändra status på ODR-registret till att inaktivera den röda LED-pinsen...
+                printf("The RED Led is set OFF \n\r"); //...och skriv ut att den röda LED är OFF.
+            }
+        break;
+        //Upprepa samma princip för övriga färger.
+        case YELLOW:
+            LED_PORT->MODER |= LED_YELLOW_MODE_BIT;
+            if(me->state == ON){
+                LED_PORT->ODR |= LED_YELLOW_PIN;
+                printf("The YELLOW Led is set ON \n\r");
+            }
+            else{
+                LED_PORT->ODR &= ~LED_YELLOW_PIN;
+                printf("The YELLOW Led is set OFF \n\r");
+            }
+        break;
 
-			}
-		break;
-		
-		case GREEN://Sätta portläget för LED-konfigurationen till output
-			LED_PORT->MODER |=LED_GREEN_MODE_BIT;
-		  if(me->state  == ON){//Sätter på LED
-			   LED_PORT->ODR |= LED_GREEN_PIN;
-				printf("The GREEN Led is set ON \n\r");
-			}
-			else{//Stänger av LED
-			  LED_PORT->ODR &= ~LED_GREEN_PIN;
-        printf("The GREEN Led is set OFF \n\r");
+        case BLUE:
+            LED_PORT->MODER |= LED_BLUE_MODE_BIT;
+            if(me->state == ON){
+                LED_PORT->ODR |= LED_BLUE_PIN;
+                printf("The BLUE Led is set ON \n\r");
+            }
+            else{
+                LED_PORT->ODR &= ~LED_BLUE_PIN;
+                printf("The BLUE Led is set OFF \n\r");
+            }
+        break;
 
-			}
-		break;
-	}
+        case GREEN:
+            LED_PORT->MODER |= LED_GREEN_MODE_BIT;
+            if(me->state == ON){
+                LED_PORT->ODR |= LED_GREEN_PIN;
+                printf("The GREEN Led is set ON \n\r");
+            }
+            else{
+                LED_PORT->ODR &= ~LED_GREEN_PIN;
+                printf("The GREEN Led is set OFF \n\r");
+            }
+        break;
+    }
 }
+//Funktion för att ställa in statusen för en LED-lampa.
+void Led_setState(Led_Type *const me,LedState_Type _state) {
+    
+    //Sätter önskad status på LED.
+    me->state =  _state;
 
-void Led_setState(Led_Type * const me,LedState_Type _state){
-	//Sätta statusen av LED:n
-	 me->state =  _state;
+    //Switch-sats med färgen som en villkor för att avgöra vilken LED-lampa som ska manipuleras.
+    switch(me->color){
+        case RED: //Om LED har färgen röd...
+            LED_PORT->MODER |= LED_RED_MODE_BIT; //...ställ in moder-registret så att den pin som är kopplad till den röda LED-lampan sätts till utgångsläge (output).
+            if(me->state == ON){ //Om önskad status är ON...
+                LED_PORT->ODR |= LED_RED_PIN; //...ställ in pin output till aktiv för att tända den röda LED-lampan...
+                printf("The RED Led is set ON\n\r"); //...och skriv ut att den röda LED:n är påslagen.
+            }
+            else{ //Annars, om önskad status är OFF...
+                LED_PORT->ODR &= ~LED_RED_PIN; //...ställ in pin output till inaktiv för att släcka den röda LED-lampan...
+                printf("The RED Led is set OFF\n\r"); //...och skriv ut att den röda LED:n är OFF.
+            }
+        break;
+        //Upprepa samma princip för övriga färger.
+        case YELLOW:
+            LED_PORT->MODER |= LED_YELLOW_MODE_BIT;
+            if(me->state == ON){
+                LED_PORT->ODR |= LED_YELLOW_PIN;
+                printf("The YELLOW Led is set ON\n\r");
+            }
+            else{
+                LED_PORT->ODR &= ~LED_YELLOW_PIN;
+                printf("The YELLOW Led is set OFF\n\r");
+            }
+        break;
 
-	//Kolla fären på LED för att veta att korrekt LED manipuleras
-	switch(me->color){
-		 //om LED Röd
-		case RED:
-			LED_PORT->MODER |=LED_RED_MODE_BIT; //Sätta pin till outputläge
-		  if(me->state  == ON){ //Om önskad status är ON
-			   LED_PORT->ODR |= LED_RED_PIN; //Definiera pinsens output till aktiv
-				printf("The RED Led is set ON \n\r");
-			}
-			else{
-			  LED_PORT->ODR &= ~LED_RED_PIN; //Definiera pin output till inaktiv
-        printf("The RED Led is set OFF \n\r");
+        case BLUE:
+                LED_PORT->MODER |= LED_BLUE_MODE_BIT;
+            if(me->state == ON){
+                LED_PORT->ODR |= LED_BLUE_PIN;
+                printf("The BLUE Led is set ON\n\r");
+            }
+            else{
+                LED_PORT->ODR &= ~LED_BLUE_PIN;
+                printf("The BLUE Led is set OFF\n\r");
+            }
+        break;
 
-			}
-		break;
-		
-		case YELLOW:
-			LED_PORT->MODER |=LED_YELLOW_MODE_BIT;//Sätta pin till outputläge
-		  if(me->state  == ON){//Om önskad status är ON
-			   LED_PORT->ODR |= LED_YELLOW_PIN;//Definiera pinsens output till aktiv
-				printf("The YELLOW Led is set ON \n\r");
-			}
-			else{
-			  LED_PORT->ODR &= ~LED_YELLOW_PIN;//Definiera pin output till inaktiv
-        printf("The YELLOW Led is set OFF \n\r");
-
-			}
-		break;
-		
-		case BLUE:
-			LED_PORT->MODER |=LED_BLUE_MODE_BIT;//Sätta pin till outputläge
-		  if(me->state  == ON){//Om önskad status är ON
-			   LED_PORT->ODR |= LED_BLUE_PIN;//Definiera pinsens output till aktiv
-				printf("The BLUE Led is set ON \n\r");
-			}
-			else{
-			  LED_PORT->ODR &= ~LED_BLUE_PIN;//Definiera pin output till inaktiv
-        printf("The BLUE Led is set OFF \n\r");
-
-			}
-		break;
-		
-		case GREEN:
-			LED_PORT->MODER |=LED_GREEN_MODE_BIT;//Sätta pin till outputläge
-		  if(me->state  == ON){//Om önskad status är ON
-			   LED_PORT->ODR |= LED_GREEN_PIN;//Definiera pinsens output till aktiv
-				printf("The GREEN Led is set ON \n\r");
-			}
-			else{
-			  LED_PORT->ODR &= ~LED_GREEN_PIN;//Definiera pin output till inaktiv
-        printf("The GREEN Led is set OFF \n\r");
-
-			}
-		break;
-	
-	}
+        case GREEN:
+            LED_PORT->MODER |= LED_GREEN_MODE_BIT;
+            if(me->state == ON){
+                LED_PORT->ODR |= LED_GREEN_PIN;
+                printf("The GREEN Led is set ON\n\r");
+            }
+            else{
+                LED_PORT->ODR &= ~LED_GREEN_PIN;
+                printf("The GREEN Led is set OFF\n\r");
+            }
+        break;
+    }
 }
+//Funktion för att kontrollera vilken status (av/på) en LED-lampa har. 
+LedState_Type Led_getState(Led_Type *const me) {
 
-LedState_Type Led_getState(Led_Type * const me){
+    //Switch-sats som kontollerar färgen på den LED-lampa som efterfrågas och printar sedan ut statusen.
+    switch(me->color){
+        case RED:
+            printf("The RED Led is currently %d\n\r", me->state);
+        break;
 
-	//Kontollera färgen av den LED som efterfrågas, printa sedan statusen
-	switch(me->color){
-		
-		case RED:
-			   printf("The RED Led is cureently %d \n\r",me->state);
-				 break;
-	  	case GREEN:
-			   printf("The GREEN Led is cureently %d \n\r",me->state);
-				 break;
-		case BLUE:
-			   printf("The BLUE Led is cureently %d\n\r",me->state);
-				 break;
-		case YELLOW:
-			   printf("The YELLOW Led is cureently  %d \n\r",me->state);
-				 break;
-	 }
-  
-	return me->state; //Returnerar LED-lampas status av vald färg
+        case GREEN:
+            printf("The GREEN Led is currently %d\n\r", me->state);
+        break;
+
+        case BLUE:
+            printf("The BLUE Led is currently %d\n\r", me->state);
+        break;
+
+        case YELLOW:
+            printf("The YELLOW Led is currently %d\n\r", me->state);
+        break;
+    }
+
+    //Returnerar LED-lampas status av vald färg.
+    return me->state; 
 }
--- Code/LED.h
+++ Code/LED.h
@@ -1,51 +1,51 @@
-#ifndef LED_H
-#define LED_H // Initierar att vår headerfil börjar
+#ifndef LED_H //Denna headerfil knyter samman LED.c och Main.c
+#define LED_H //Initierar att vår headerfil börjar.
 
-#include <stdint.h> // ger oss heltalstyper med exakt bredd oberoende av underliggande hårdvara (int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t)
-#include "UART.h" // inkluderar vår uartHeader
-#include "stm32f4xx.h" //Device Header för enhetsspecifika angivelser om hårdvaran
+#include <stdint.h> //Inkludera standardbibliotek för C. Ger oss heltalstyper med exakt storlek oberoende av underliggande hårdvara (int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t).
+#include "UART.h" //Inkluderar vår uart headerfil, vår drivrutin.
+#include "stm32f4xx.h" //Device Header för enhetsspecifika angivelser om hårdvaran.
 
-//Definiera vilken GPIO som skall vara ansvarig för LED-funktionen
-#define	LED_PORT GPIOB
+//Definiera vilken GPIO som skall vara ansvarig för LED-funktionen. Alla gånger headern inkluderas och LED_PORT skrivs så är detta synonymt med GPIOB.
+#define LED_PORT GPIOB
 
-//Definiera klocksignalen för porten
-#define	LED_PORT_CLOCK (1U<<1)
+//Definiera klocksignalen för porten. vänsterskiftar LED_PORT_CLOCK från 1 (00000001) till 2 (00000010). Detta motsvarar den bit som styr klocksignalen.
+#define LED_PORT_CLOCK (1U<<1)
 
-//Definiera de olika pinsen för de olika LED-färgerna
-#define LED_RED_PIN	(1U<<14)
+//Definiera bitpositionerna för de GPIO-pins som är kopplade till LED-lamporna på mikrokontrollern.
+#define LED_RED_PIN	(1U<<14) //Definiera den 14:e biten i registeradressen för att styra röda LED-lampan (dvs. den 14:e biten är satt till 1 och alla andra bitar är satta till 0). Samma princip på övriga nedan. 
 #define LED_GREEN_PIN (1U<<12)
 #define	LED_BLUE_PIN (1U<<15)
 #define LED_YELLOW_PIN (1U<<13)
 
-//Definiera mode bits för varje LED-färg
-#define LED_RED_MODE_BIT (1U<<28)
+//Definiera mode bits för varje LED-färg.
+#define LED_RED_MODE_BIT (1U<<28) //Definierar bitpositionen 28 som motsvarar en röd LED-lampa. Samma princip för övriga nedan.
 #define LED_GREEN_MODE_BIT (1U<<24)
 #define LED_YELLOW_MODE_BIT (1U<<26)
 #define LED_BLUE_MODE_BIT (1U<<30)
 
-//Definiera de olika LED-färgerna som finns
-typedef  enum {
-	RED	= 0,
-	GREEN,
-	YELLOW,
-	BLUE
-}LedColor_Type;
+//Definiera de olika LED-färgerna som finns.
+typedef enum { //Skapar en ny datatyp som kallas LedColor_Type och kan innehålla fyra olika värden som representerar färgerna för de olika LED-lamporna.
+    RED = 0, //Röd börjar på 0, grön är 1 osv. 
+    GREEN,
+    YELLOW,
+    BLUE
+}LedColor_Type; //Denna nya datatyp kan användas för att skapa variabler som representerar en av dessa fyra färger.
 
-//Definierar/numrerar de olika lägena som LED kan befinna sig i
+//Definierar LED-lampans status, om den är av eller på.
 typedef enum {
-	OFF =0,
-	ON = 1
-}LedState_Type;
+    OFF = 0,
+    ON = 1
+}LedState_Type;  //Denna datatyp kallar vi för LedState_Type. Samma princip gäller som i ovan enum.
 
-//Definierar de attribut som LED bestå utav
-typedef struct {
-	LedColor_Type color; 
-	LedState_Type state;  
-}Led_Type;
+//Struct som definierar de attribut som en LED-lampa består utav.
+typedef struct { //Innehåller två medlemmar som representerar färgen och statusen för en LED-lampa.
+    LedColor_Type color; 
+    LedState_Type state;  
+}Led_Type; //Ges namnet Led_Type som blir en egen datatyp för variabler som kan lagra info om färg och status för en LED-lampa.
 
-//Deklarerar funktionerna för LED-konstruktion och statuskontrollen
-void Led_ctor(Led_Type * const me, LedColor_Type _color, LedState_Type _state);
-void Led_setState(Led_Type * const me,LedState_Type _state);
-LedState_Type Led_getState(Led_Type * const me);
+//Deklarera funktionerna för LED-konstruktorn, statusangivelse och statuskontroll 
+void Led_ctor(Led_Type *const me, LedColor_Type _color, LedState_Type _state); //Konstruktorn. Skapar en ny instans av structen Led_Type och sätter dess initiala värden, t.ex. att den ska vara röd och påslagen.
+void Led_setState(Led_Type *const me, LedState_Type _state); //Uppdaterar LED-lampans status
+LedState_Type Led_getState(Led_Type *const me); //Hämtar LED-lampans nuvarande status
 
-#endif // förtydligar avslutningen av vår headerfil
+#endif //Anger att headerfilen avslutas
