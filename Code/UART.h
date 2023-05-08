4ce074ec8ee95eda8373615cafae1940460838ef
mmyrberg <m.myrberg@outlook.com>
2023-05-08 12:34:34 +0200

    Update comments in UART

--- Code/UART.h
+++ Code/UART.h
@@ -2,7 +2,7 @@
 #ifndef UART_H //Kolla om __UART_H är definierat.
 #define UART_H //Om inte så definieras det och all kod som följer inkluderas.
 
-#include "stm32f4xx.h" //Inkludera biblioteket för denna mikrokontroller så att vi får tillgång till den källkod och hänvisningar vi behöver för arbete på STM-hårdvara.   
+#include "stm32f4xx.h" //Inkludera biblioteket för denna mikrokontroller så att vi får tillgång till den källkod och hänvisningar vi behöver för arbete på STM-hårdvara.
 #include <stdio.h> //Inkluderar standard I/O för C. Möjliggör användning av standardin- och utdatafunktioner som printf() och scanf().
 
 void USART2_Init(void); //Deklarera UART-funktionen i UART.c
