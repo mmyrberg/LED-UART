b061fa981b099d662ea6f6501f3deeb942440458
mmyrberg <m.myrberg@outlook.com>
2023-05-08 12:14:18 +0200

    Update comments in UART files

--- Code/UART.c
+++ Code/UART.c
@@ -1,62 +1,59 @@
-#include "UART.h" // Hämtar vår samlingsfil för headers och funktioner. inkludera headerfilen UART, källkoden från STM för deras plattform f4xx
+#include "UART.h" //Inkludera vår headerfil UART.
 
-// Definiera funktion för att initiera Usart-protokollet och dess beståndsdelar. Denna funktion är en drivrutin.
-void USART2_Init(void){ 
-    // konfiguera systemklockan (RCC) för mikrokontrollern. 
-    // Pilen "->" referear till headern "stm32f4xx.h", pekar på den, som pionters(*) fast för headerfiler.
-    // APB1ENR är ett register inom RCC som används för att aktivera eller inaktivera klockan. 
-    // När man bara sätter "=" finns risk att förkonfiguarade bitar skrivs över, därför använder man "|=", vilket gör att inget manupilueras längs vägen.
-    // Värdet 0x20000 är en hexadecimal representation av det binära värdet 0010 0000 0000 0000 0000, som har en 1 i den 17:e bitpositionen(index) och nollor i alla andra bitpositioner. 
-    // Tillsammans med "|=" innebär detta att man sätter bit 17 i APB1ENR-registret till 1 utan att påverka värdena för de andra bitarna i registret.
-    // Genom att sätta bit 17 till 1 aktiverar koden klockan för en enhet som är ansluten till APB1-bussen.
-    // 1. Enablea klocktillgång för uart2
-    RCC->APB1ENR  |=  0x20000; // Aktiverar UART2 genom att sätta bit 17 i APB1ENR till 1.
-    // 2. Enablea klocktillgång för port A
-    RCC->AHB1ENR   |=0x01; // Aktiverar GPIO genom att sätta bit 0 i AHB1ENR till 1.
-    // 3. Enablea pins relaterade till vald port, för alternativ funktion= betyder att den konverteras till att den används till. Vanliga funktionen är I/O, men när det gäller UART protokkollet är det inte längre en vanlig utan klassas. Är det vanlig I/O är inte alternativt, är det  
-    GPIOA->MODER &=~0x00F0; // Rensar bitarna 4-7 för att förbereda pins PA2 och PA3.
-     //"& samt ~ forcerar en invertering som leder till att bitarna som via hex betecknas med 1, i realvärde ersätts med 0".
-    GPIOA->MODER |= 0x00A0; // Sätter bitarna 5 och 7 till 1 fö att aktivera alternativ funtionalitet på pins PA2 och PA3
-    // 4. Välja typen av alternativ funktion för de valda pinsen, hakperenteserna syftar till att välja början av AFR-LOW/HIGH
-    GPIOA->AFR[0] &= ~0xFF00; // Rensar bitarna 8-15 för att förbereda pins PA2 och PA3.
-    GPIOA->AFR[0] |= 0x7700; // Sätter bitarna 8-11 samt 12-14 till formatet 0111
-    // Nu är pin 2 och 3 redo för att skicka och ta emot UART-kommunikation
-    /*Konstruktion av enhetens kommunikation avslutas*/
+//Definiera funktion för att initiera USART-protokollet och dess beståndsdelar. Denna funktion utgör en drivrutin.
+void USART2_Init(void) { 
+    // 1. Aktivera klocktillgång för UART2. APB1ENR är ett register inom RCC (systemklockan) som används för att aktivera eller inaktivera klockan. 
+    RCC->APB1ENR |= 0x20000; //Aktiverar klockan för UART2 genom att sätta bit 17 i APB1ENR till 1. Om man bara sätter "=" finns risk att förkonfiguarade bitar skrivs över, därför använder vi "|=".
+    // 2. Aktivera klocktillgång för port A.
+    RCC->AHB1ENR |= 0x01; //Aktiverar klockan för GPIO-port A genom att sätta bit 0 i RCC's AHB1ENR-register till 1.
+    // 3. Aktivera pins relaterade till vald port, för alternativ funktion, vilket ger GPIO-pinsen möjlighet att användas för andra syften än enkel in- och utdata (I/O), som t.ex. kommunikation med UART-protokollet.
+    GPIOA->MODER &= ~0x00F0; //Rensar bitarna 4-7 i GPIO-port A:s MODER-register för att förbereda pins PA2 och PA3 ifall dessa är i andra lägen. "&" samt "~" forcerar en invertering som leder till att bitarna som via hex betecknas med 1, i realvärde ersätts med 0.
+    GPIOA->MODER |= 0x00A0; //Sätter bitarna 5 och 7 i GPIO-port A:s MODER-register till 1 för att aktivera alternativ funtionalitet på pins PA2 och PA3.
+    // 4. Välj typen av alternativ funktion för de valda pinsen. Hakperenteserna syftar till att välja början av AFR-registret LOW/HIGH.
+    GPIOA->AFR[0] &= ~0xFF00; //Rensar bitarna 8-15 för att förbereda pins PA2 och PA3 för alternativ funktion.
+    GPIOA->AFR[0] |= 0x7700; //Sätter bitarna 8-11 samt 12-14 till formatet 0111 för att välja alternativ funktion till UART på pins PA2 och PA3.
+    /*Nu är pin 2 och 3 redo för att skicka och ta emot UART-kommunikation*/
 
-    // Konfiguration av UART
-    USART2->BRR  =  0x0683; // Sätter en standard baud-rate med hjälp av hexadecimalen 0x0683 (9600bps)
-    USART2->CR1  =  0x000C; // Sätter tx och rx till att arbeta i 8 bitars data. (8 bitars data, 1 stop bit, ingen paritet)
-    USART2->CR2  =  0x000; // Nollställer CR2
-    USART2->CR3  =  0x000; // Nollställer CR3
-    USART2->CR1  |=  0x2000; // Omställer bit 13 (Uart-aktiveringen) till 1
+    //Konfiguration av UART
+    USART2->BRR = 0x0683; //Sätter en standard baud-rate till 9600bps genom att skriva hexavärdet 0x0683 till BRR-registret.
+    USART2->CR1 = 0x000C; //Sätter transmit (tx) och receive (rx) till att arbeta i 8 bitars data (8 bitars data, 1 stop bit, ingen paritet).
+    USART2->CR2 = 0x000; //Nollställer USART2:s CR2-register.
+    USART2->CR3 = 0x000; //Nollställer USART2:s CR3-register.
+    USART2->CR1 |= 0x2000; //Omställer bit 13 i CR1-registret till 1 för att aktivera UART.
 }
-    // UART Read och Write-regler
-int USART2_write(int ch){ // Deklarerar skrivfunktionen (Överföringen av data till terminalen)
-    //En loop för att skicka byte för byte
-	while(!(USART2->SR & 0x0080)){} // Sätter ett krav som kontrollerar att statusen på överföringen är tom och kan ta emot nästa karaktär(byte)
-	USART2->DR = (ch & 0xFF); // Sätter överföringen av byten till dataregistret
-	
-  return ch;
+
+//UART write-regler.
+int USART2_write(int ch) { //Deklarerar skrivfunktionen (överföring av data till terminalen).
+    
+    //En loop för att skicka byte för byte...
+    while(!(USART2->SR & 0x0080)){} //...sätter ett krav som kontrollerar att statusen på överföringsdataregistret (bit 7) är tomt och redo för att ta emot nästa karaktär(byte).
+    
+    USART2->DR = (ch & 0xFF); //Sätter överföringen av byten till dataregistret.
+    
+    return ch; //Returnera det överförda värdet. 
 }
-    //Uart read
-int USART2_read(void){ // Deklarerar läsfunktionen (mottagning av information)
-  while(!(USART2->SR & 0x0020)){} // Sätter ett krav som kontrollerar om det finns mer data att hämta
-	return USART2->DR; // Läser ut datan
+
+//Uart read-regler.
+int USART2_read(void) { //Deklarerar läsfunktionen (mottagning av information).
+    while(!(USART2->SR & 0x0020)){} //Sätter ett krav som kontrollerar om det finns data att läsa/hämta.
+    
+    return USART2->DR; //Returnera läst datavärde.
 }
-// Interface för standard I/O i C
-// En omdiregering till att utge datan i terminalen
-struct __FILE { int handle; }; // Strukturerar våra huvudsakliga överföringsströmmar
+
+//Interface för standard I/O i C.
+//En omdiregering till att utge datan i terminalen. 
+struct __FILE { int handle; }; //Strukturerar våra huvudsakliga överföringsströmmar.
 FILE __stdin  = {0};
 FILE __stdout = {1};
 FILE __stderr = {2};
 
-
-int fgetc(FILE *f) { // fget hämtar en byte från standardströmmen och behandlar även teckenreturer
+//Funktion för att läsa en byte från standardinmatningen. 
+int fgetc(FILE *f) { //fgetc hämtar en byte från standardströmmen och behandlar även teckenreturer.
     int c;
 
     c = USART2_read();     
 
-    if (c == '\r') {        
+    if(c == '\r'){        
         USART2_write(c);    
         c = '\n';
     }
@@ -66,23 +63,22 @@ int fgetc(FILE *f) { // fget hämtar en byte från standardströmmen och behandl
     return c;
 }
 
-int fputc(int c, FILE *f) { // fput skriver en byte till standardströmmen
+//Funktion för att skriva ut till terminalen.
+int fputc(int c, FILE *f) { //fputc skriver en byte till standardströmmen.
     return USART2_write(c);
 }
 
+int n; //Deklarerar en byte för användning i testfunktionen.
+char str[80]; //Sätter en begränsning av antal karaktärer i överföringen.
 
-int n; // Deklarerar en byte för användning i testfunktionen
-char str[80]; // Sätter en limitering i överföringen av karaktärer
-		
-void test_setup(void){ // Testar vår läs- och skrivfunktion
-	
-	printf("please enter a number: ");
-	scanf("%d", &n);
-	printf("the number entered is: %d\r\n", n);
-	printf("please type a character string: ");
-	gets(str);
-	printf("the character string entered is: ");
-	puts(str);
-	printf("\r\n");
-	
+//Funktion som testar läs- och skrivfunktionen.
+void test_setup(void) {
+    printf("please enter a number: ");
+    scanf("%d", &n);
+    printf("the number entered is: %d\r\n", n);
+    printf("please type a character string: ");
+    gets(str);
+    printf("the character string entered is: ");
+    puts(str);
+    printf("\r\n");
 }
--- Code/UART.h
+++ Code/UART.h
@@ -1,11 +1,11 @@
-// Denna fil knyter samman UART.c och Main.c 
-ifndef  __UART_H
-#define  __UART_H // Initierar att vår headerfil börjar
+//Denna fil knyter samman UART.c och Main.c 
+#ifndef UART_H //Kolla om __UART_H är definierat.
+#define UART_H //Om inte så definieras det och all kod som följer inkluderas.
 
-#include "stm32f4xx.h" // Hämtar källkoden och hänvisningar för arbete på STM-hårdvara. 
-#include <stdio.h> //  Inkluderar standard I/O för C
+#include "stm32f4xx.h" //Inkludera biblioteket för denna mikrokontroller så att vi får tillgång till den källkod och hänvisningar vi behöver för arbete på STM-hårdvara.   
+#include <stdio.h> //Inkluderar standard I/O för C. Möjliggör användning av standardin- och utdatafunktioner som printf() och scanf().
 
-void USART2_Init(void); // Kallar på och refererar till deklarationen av vår UART-funktion
-void test_setup(void); //Kallar på och refererar till vår testfunktion ur UART.c
+void USART2_Init(void); //Deklarera UART-funktionen i UART.c
+void test_setup(void); //Deklarera testfunktionen i UART.c
 
-#endif // förtydligar avslutningen av vår headerfil
+#endif //Markerar avslutningen av denna headerfil.
